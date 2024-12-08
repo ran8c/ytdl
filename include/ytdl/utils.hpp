4b86243755c11248483dedf9fcc5471ea8f405f3
ran8c <ran8c@icloud.com>
2024-12-07 22:05:24 -0800

    Add ytdl::utils library

---
+++ include/ytdl/utils.hpp
@@ -0,0 +1,19 @@
+#ifndef YTDL_YTDL_UTILS_HPP
+#define YTDL_YTDL_UTILS_HPP
+
+#include <string>
+#include <vector>
+
+namespace ytdl::utils {
+
+/**
+ * @brief Creates a string representation of a string vector.
+ * @param `std::vector<std::string>&`: The vector to represent.
+ *
+ * This is intended for print debugging.
+ */
+std::string rep_str_vec(const std::vector<std::string>&);
+
+}
+
+#endif
---
+++ src/utils.cpp
@@ -0,0 +1,21 @@
+#include "ytdl/utils.hpp"
+
+#include <fmt/format.h>
+
+using namespace ytdl;
+
+std::string utils::rep_str_vec(const std::vector<std::string>& vec) {
+    // [item, item, item]
+    
+    std::string output{"["};
+    for (const auto& str: vec) {
+        output.append(fmt::format("{}, ", str));
+    }
+    // remove last comma and space
+    if (output.size() > 1) {
+        output.resize(output.size() - 2);
+    }
+    output.append("]");
+
+    return output;
+}
