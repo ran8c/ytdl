951e0440041f82006413cac74cc00591721d94ab
ran8c <ran8c@icloud.com>
2024-12-08 16:08:40 -0800

    Document ytdl::preset_docs

--- include/ytdl/presets.hpp
+++ include/ytdl/presets.hpp
@@ -7,19 +7,44 @@
 
 namespace ytdl::presets {
 
+/**
+ * A configured preset for the downloader.
+ */
 struct preset {
     std::string name;
     std::string desc;
     std::vector<std::string> flags;
 };
 
+/**
+ * @brief Class storing a list of supported presets.
+ */
 class supported_presets {
    private:
+    /**
+     * The internal set of supported presets.
+     */
     std::vector<preset> preset_list;
 
    public:
+    /**
+     * @brief Store a list of presets.
+     * @param `std::vector<preset>&`: the list of presets to store.
+     */
     explicit supported_presets(const std::vector<preset>&);
+
+    /**
+     * @brief Get a stored preset.
+     * @param `std::string_view&`: the name of a preset to get.
+     * @throw `std::invalid_argument` if no preset with given name was found.
+     */
     preset& get_preset(const std::string_view&);
+
+    /**
+     * @brief Print out the list of supported presets.
+     *
+     * This is intended for debugging.
+     */
     void print();
 };
 
--- src/presets.cpp
+++ src/presets.cpp
@@ -11,9 +11,16 @@
 
 using namespace ytdl;
 
+/**
+ * Constructor for `presets::supported_presets`.
+ */
 presets::supported_presets::supported_presets(const std::vector<preset>& presets) : preset_list(presets) {}
 
+/**
+ * Getter for a `preset` in `presets::supported_presets.preset_list`.
+ */
 presets::preset& presets::supported_presets::get_preset(const std::string_view& selected_preset) {
+    // `std::find_if()` exists, but this method is easier for me to read
     for (auto& preset : this->preset_list) {
         if (preset.name == selected_preset) {
             return preset;
@@ -23,6 +30,9 @@ presets::preset& presets::supported_presets::get_preset(const std::string_view& 
     throw std::invalid_argument(exception);
 }
 
+/**
+ * Debug print out `presets::supported_presets.preset_list`.
+ */
 void presets::supported_presets::print() {
     for (const auto& preset : this->preset_list) {
         std::cout << fmt::format("[{}, {}]\n", preset.name, utils::rep_str_vec(preset.flags));
